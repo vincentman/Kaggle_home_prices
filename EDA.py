c56c2481477b7d216d2faffa782b825e4faec06e
vincentkao <vincentkao@ibms.sinica.edu.tw>
2018-05-11 15:22:16 +0800

    EDA

--- EDA.py
+++ EDA.py
@@ -12,7 +12,24 @@ k = 10  # number ofvariables for heatmap
 cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
 cm = np.corrcoef(df_train[cols].values)
 
-total= df_train.isnull().sum().sort_values(ascending=False)
-percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
-missing_data = pd.concat([total, percent], axis=1, keys=['Total','Percent'])
-missing_data.head(20)
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum() / df_train.isnull().count()).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+print(missing_data.head(20))
+
+df_train = df_train.drop((missing_data[missing_data['Total'] > 1]).index, 1)
+print(df_train.loc[df_train['Electrical'].isnull()].index)
+df_train = df_train.drop(df_train.loc[df_train['Electrical'].isnull()].index)
+saleprice_scaled = StandardScaler().fit_transform(df_train['SalePrice'][:, np.newaxis])
+low_range = saleprice_scaled[saleprice_scaled[:, 0].argsort()][:10]
+high_range = saleprice_scaled[saleprice_scaled[:, 0].argsort()][-10:]
+print('outer range (low) of the distribution:')
+print(low_range)
+print('\nouter range (high) of the distribution:')
+print(high_range)
+
+ids = df_train.sort_values(by = 'GrLivArea',ascending = False)[:2]['Id']
+# df_train = df_train.drop(df_train[df_train['Id'] == 1299].index)
+# df_train = df_train.drop(df_train[df_train['Id'] == 524].index)
+df_train = df_train.drop(ids.index)
+print(df_train)
